d31dbbb6d033edc583dc25ba881706a854f34d91
Wassim Moussawi <wassim.moussawi001@gmail.com>
2021-10-13 13:24:19 +0200

    Added the bt preprocessing

--- edgeListGenerator.py
+++ edgeListGenerator.py
@@ -6,7 +6,7 @@ import pandas
 # ['user_a', 'user_b', 'fb', 'bt', 'sms', 'calls', 'weight']
 #  - fb: 1 if user a and b are facebook friends, 0 otherwise
 #  - sms: The number of sms sent between user a and b
-#  
+#  - calls: The total number of seconds that user a and b have been calling each other
 # TODO: CALCULATE THE REMAINING ATTRIBUTES
 def rm_main(fbFriends, gender, bt, sms, calls):
     
@@ -21,6 +21,8 @@ def rm_main(fbFriends, gender, bt, sms, calls):
 
     fbEdges(fbFriends, edges)
     smsEdges(sms, edges)
+    callsEdges(calls, edges)
+    btEdges(bt, edges)
     
     df = edges[0]
     for i in range(1, maxID):
@@ -28,6 +30,39 @@ def rm_main(fbFriends, gender, bt, sms, calls):
 
     return df
 
+# Processes the calls. For every call between user a and b that
+# lasts t seconds, the edge between user 
+# a and b will get +t in the 'calls' column
+def btEdges(bt, edges):
+    for row in range(0, bt.shape[0]):
+        user_a = bt.at[row, 'user_a']
+        user_b = bt.at[row, 'user_b']
+        rssi = bt.at[row, 'rssi']
+
+        if (user_a == user_b or user_b==-1 or rssi == 0 or user_a == -2 or user_b==-2):
+            continue
+        distance = 10 ** ((-60 -(rssi))/(10 * 2))
+        if distance <= 5:
+	        lesser = min(user_a, user_b)
+	        greater = max(user_a, user_b)
+	        addEdge(edges, lesser, greater, 'bt', 1)
+
+# Processes the calls. For every call between user a and b that
+# lasts t seconds, the edge between user 
+# a and b will get +t in the 'calls' column
+def callsEdges(calls, edges):
+    for row in range(0, calls.shape[0]):
+        caller = calls.at[row, 'caller']
+        callee = calls.at[row, 'callee']
+        duration = calls.at[row, 'duration']
+
+        if (caller == callee):
+            continue
+        if (duration < 0):
+        	  continue
+        lesser = min(caller, callee)
+        greater = max(caller, callee)
+        addEdge(edges, lesser, greater, 'calls', duration)
 
 # Processes the sms data. For every message between user a and b,
 # no matter who is the sender and recipient, the edge between user 
@@ -61,6 +96,15 @@ def fbEdges(fbFriends, edges):
         addEdge(edges, lesser, greater, 'fb', 1)
 
 
+def column_contribution(column):
+    return {
+        'fb':1, 
+        'bt':1,
+        'sms':1,
+        'calls': 1
+    }[column]
+
+
 # Adds a relation between two users to edges
 #  - If those users already have a connecting edge, the values of the edge are modified. 
 #    The provided value is added to the correct attribute column of the edge.
@@ -75,14 +119,15 @@ def addEdge(edges, userA, userB, columnLabel, value):
     userEdges = edges[userA]
     rowUserB = userEdges.index[userEdges['user_b']==userB].tolist()
 
-    if (len(rowUserB) > 0):
-        row = rowUserB[0]
-        edges[userA].at[row, columnLabel] += value
-    else:
-        rows = userEdges.shape[0]
-        newRow = [userA, userB, 0, 0, 0, 0, 0]
-        newRow[labelIndex(columnLabel)] = value
-        edges[userA].loc[rows+1] = newRow
+    if (len(rowUserB) == 0):
+        newRow = {'user_a': userA, 'user_b': userB, 'fb':0, 'bt':0, 
+        'sms':0, 'calls': 0, 'weight':0}
+        edges[userA] = edges[userA].append(newRow, ignore_index=True)
+        rowUserB = edges[userA].index[edges[userA]['user_b']==userB].tolist()
+    
+    row = rowUserB[0]
+    edges[userA].at[row, columnLabel] += value
+    edges[userA].at[row, 'weight'] += column_contribution(columnLabel)*value
       
 
 # Gets the index of a column in 
@@ -143,3 +188,4 @@ def rm_main(gender, fbFriends):
 
     return df
 """
+
